b9b70165fdb10d3883aa7bfa81cc75dd810f9f0f
TravlingCoder <travlingCoder@gmail.com>
2020-09-23 20:13:34 -0700

    Version 1.0
    Sprites need names, needs to fetch pictures, color and slight ui changes.
    Almost done

--- client/src/components/pokemon.js
+++ client/src/components/pokemon.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './pokemon.css';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Table, Container } from 'react-bootstrap';
 
 export class Pokemon extends Component {
   constructor(props) {
@@ -8,73 +8,131 @@ export class Pokemon extends Component {
     this.state = {
       team1: [],
       team2: [],
-      pokemon: []
+      pokemon: [],
+      randomMove: [],
+      team1Random: [],
+      team2Random: []
     };
   }
 
   spinAgain() { //Need to fix chances of duplicate pokemon
     fetch('/api/pokemon')
     .then(res => res.json())
-    .then(pokemon => this.setState({pokemon: pokemon}, 
-      () => this.setState(state => {
+    .then(pokemon => this.setState({pokemon: pokemon.pokemon},
+      this.setState({randomMove: pokemon.randomMove}),
+      this.setState(state => {
         if(state.team1.length === state.team2.length){
           const team1 = []; 
+          const team1Random = [];
           for(var i = 0; i < state.team1.length; i++){
             team1.push(state.team1[i])
           }
+          for(var z = 0; z < state.team1Random.length; z++){
+            team1Random.push(state.team1Random[z])
+          }
+          team1Random.push(state.randomMove)
           team1.push(state.pokemon[0]) 
-          return {team1}
+          return {team1, team1Random}
         }else{
           const team2 = []; 
+          const team2Random = [];
+          console.log(team2Random);
           for(var n = 0; n < state.team2.length; n++){
             team2.push(state.team2[n])
           }
+          for(var p = 0; p < state.team2Random.length; p++){
+            team2Random.push(state.team2Random[p])
+          }
+          team2Random.push(state.randomMove)
           team2.push(state.pokemon[0]) 
-          return {team2}
+          return {team2, team2Random}
         }
       })
     ));
   }
 
+  componentDidUpdate() {
+      if(this.state.team1.length === 6 && this.state.team2.length === 6){
+        var f = document.getElementById('button');
+        f.style.display = "none";
+    }
+  }
+
   componentDidMount() {
     fetch('/api/pokemon')
       .then(res => res.json())
-      .then(pokemon => {this.setState({pokemon})
-                        this.setState({team1: pokemon})})
+      .then(pokemon => {this.setState({pokemon: pokemon.pokemon})
+                        this.setState({team1: pokemon.pokemon})
+                        this.setState({randomMove: pokemon.randomMove})
+                        const team1Random = [];
+                        team1Random.push(pokemon.randomMove);
+                        this.setState({team1Random});
+      })
   }
 
   render() {
     return (
-      <div>
+      <Container>
         <Row>
+          <Container>
+            <Col>
+              <h2>Pokemon</h2>
+              <ul>
+                {this.state.pokemon.map(pokemon => 
+                  <li key={pokemon.id}> {pokemon.name} {pokemon.galar_dex} </li>
+                )}
+              </ul>
+              <br/>
+
+              <button id='button' onClick={() => this.spinAgain()}>Spin Again</button>
+            </Col>
+          </Container>
           <Col>
             <h2>Team 1</h2>
-            <ul>
-              {this.state.team1.map(team1 => 
-                <li key={team1.id}>{team1.name} {team1.galar_dex}</li>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Pokemon</th>
+                  <th colSpan="4">Moves</th>
+                </tr>
+              </thead>
+              {this.state.team1Random.map(team1Random => 
+                <tbody>
+                  <tr>
+                    <td>{team1Random[0]}</td>
+                    <td>{team1Random[1][1]}</td>
+                    <td>{team1Random[2][1]}</td>
+                    <td>{team1Random[3][1]}</td>
+                    <td>{team1Random[4][1]}</td>
+                  </tr>
+                </tbody>
               )}
-            </ul>
-          </Col>
-          <Col>
-            <h2>Pokemon</h2>
-            <ul>
-              {this.state.pokemon.map(pokemon => 
-                <li key={pokemon.id}>{pokemon.name} {pokemon.galar_dex}</li>
-              )}
-            </ul>
-
-            <button onClick={() => this.spinAgain()}>Spin Again</button>
+            </Table>
           </Col>
           <Col>
             <h2>Team 2</h2>
-              <ul>
-                {this.state.team2.map(team2 => 
-                 <li key={team2.id}>{team2.name} {team2.galar_dex}</li>
-                )}
-              </ul>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Pokemon</th> 
+                  <th colSpan="4">Moves</th>
+                </tr>
+              </thead>
+              {this.state.team2Random.map(team2Random => 
+                <tbody>
+                  <tr>
+                    <td>{team2Random[0]}</td>
+                    <td>{team2Random[1][1]}</td>
+                    <td>{team2Random[2][1]}</td>
+                    <td>{team2Random[3][1]}</td>
+                    <td>{team2Random[4][1]}</td>
+                  </tr>
+                </tbody>
+              )}
+            </Table>
           </Col>
         </Row>
-      </div>
+      </Container>
     );
   }
 }
--- server.js
+++ server.js
@@ -8,7 +8,22 @@ app.get('/api/pokemon', (req, res) => {
   var pokemon2 = JSON.parse(rawdata);
   var randomNum = Math.round(Math.random() * pokemon2.length);
   pokemon.push(pokemon2[randomNum]);
-  res.json(pokemon);
+  var randomMove = [];
+  var moveCount = [];
+  for(i=0; i<4; i++){
+    randomMove.push(pokemon[0].level_up_moves[Math.round(Math.random() * pokemon[0].level_up_moves.length)]);
+    for(x=-1; x < moveCount.length;x++){
+      while(randomMove[i] === randomMove[x] || randomMove[i] === undefined){
+        randomMove.splice(-1, 1);
+        randomMove.push(pokemon[0].level_up_moves[Math.round(Math.random() * pokemon[0].level_up_moves.length)]);
+      }
+    }
+    moveCount.push(i);
+  }
+  randomMove.unshift(pokemon[0].name)
+  console.log(pokemon[0].name)
+  res.json({pokemon,
+            randomMove});
 });
 
 const port = 5000;
